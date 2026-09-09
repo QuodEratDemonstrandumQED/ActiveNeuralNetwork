09fed0d729f999290ebde4aa6ad66adfa60278fc
Quod Erat Demonstrandum <69770294+QuodEratDemonstrandumQED@users.noreply.github.com>
2026-09-09 15:42:54 +0000

    some changes, adding initial neuron connections

--- ActiveNeuralNetwork.hpp
+++ ActiveNeuralNetwork.hpp
@@ -1,11 +1,14 @@
-//
-// Created by troyg on 23/08/2026.
-//
+/**
+ * Created by Quod Erat Demonstrandum on 23/08/2026.
+*/
 
 #ifndef QED_ACTIVENEURALNETWORK_HPP
 #define QED_ACTIVENEURALNETWORK_HPP
 
 #include <vector>
+#include <random>
+#include <unordered_map>
+#include <algorithm>
 
 
 
@@ -21,7 +24,7 @@ public:
     float Weight;
     float MyelinSheath;
     std::vector<float[2]> Signals;
-    AxonTerminalComponent(Neuron*& in, Neuron*& out) : In(in), Out(out), Weight(1), MyelinSheath(1) { }
+    AxonTerminalComponent(Neuron* in, Neuron* out) : In(in), Out(out), Weight(1), MyelinSheath(1) { }
 };
 
 typedef std::vector<AxonTerminalComponent> AxonComponent;
@@ -46,10 +49,42 @@ class NeuralNetwork {
     std::vector<Neuron> In;
     std::vector<Neuron> Out;
     std::vector<Neuron> Hidden;
+public:
     NeuralNetwork(int in, int out, int hidden) {
         for (int i = 0; i < in; i++) In.emplace_back(InputNeuron);
         for (int i = 0; i < hidden; i++) Hidden.emplace_back(HiddenNeuron);
         for (int i = 0; i < out; i++) Out.emplace_back(OutputNeuron);
+
+        std::unordered_map<int, std::vector<int>> InConnections;
+        std::unordered_map<int, std::vector<int>> Connections;
+        std::unordered_map<int, std::vector<int>> OutConnections;
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(0, hidden-1);
+        for (int i = 0; i < in; ++i) {
+            for (int j = 0; j <= std::min(10*in, hidden/5); ++j) {
+                int r = dist(gen);
+                if (std::find(InConnections[i].begin(), InConnections[i].end(), r) != InConnections[i].end()) j--;
+                else InConnections[i].push_back(r);
+            }
+        }
+        for (int i = 0; i < hidden; ++i) {
+            for (int j = 0; j <= hidden/100; ++j) {
+                int r = dist(gen);
+                if (std::find(Connections[i].begin(), Connections[i].end(), r) != Connections[i].end() || std::find(Connections[r].begin(), Connections[r].end(), i) != Connections[r].end()) j--;
+                else Connections[i].push_back(r);
+            }
+        }
+        for (int i = 0; i < in; ++i) {
+            for (int j = 0; j <= std::min(10*out, hidden/5); ++j) {
+                int r = dist(gen);
+                if (std::find(OutConnections[i].begin(), OutConnections[i].end(), r) != OutConnections[i].end()) j--;
+                else OutConnections[i].push_back(r);
+            }
+        }
+
+        for (int i = 0; i < in; i++) for (int t : InConnections[i]) In[i].Axon.push_back(AxonTerminalComponent(&In[i], &Hidden[t]));
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 #include "ActiveNeuralNetwork.hpp"
 
 int main() {
+    NeuralNetwork NN = NeuralNetwork(5, 5, 25);
     return 0;
 }
