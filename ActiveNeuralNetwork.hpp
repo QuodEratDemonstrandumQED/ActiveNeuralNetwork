3d64aec579497c19d25691c96b4cb34dc4d667c9
Quod Erat Demonstrandum <69770294+QuodEratDemonstrandumQED@users.noreply.github.com>
2026-09-10 13:28:22 +0000

    Importing, Exporting - probably

--- ActiveNeuralNetwork.hpp
+++ ActiveNeuralNetwork.hpp
@@ -5,6 +5,9 @@
 #ifndef QED_ACTIVENEURALNETWORK_HPP
 #define QED_ACTIVENEURALNETWORK_HPP
 
+#include <iostream>
+#include <sstream>
+#include <fstream>
 #include <vector>
 #include <random>
 #include <unordered_map>
@@ -42,6 +45,7 @@ public:
     float ActivationThreshold;
     float LeakRate;
     Neuron(NeuronType type) : Type(type), Excitation(0), ActivationThreshold(1), LeakRate(1) { }
+    Neuron(NeuronType type, float ActivationThreshold, float LeakRate) : Excitation(0) { }
 };
 
 
@@ -88,7 +92,103 @@ public:
         for (int i = 0; i < hidden; i++) for (int t : Connections[i]) Hidden[i].Axon.push_back(AxonTerminalComponent(&Hidden[i], &Hidden[t]));
         for (int i = 0; i < out; i++) for (int t : OutConnections[i]) Hidden[t].Axon.push_back(AxonTerminalComponent(&Hidden[t], &Out[i]));
     }
+    NeuralNetwork(std::string fpath) {
+        std::ifstream File(fpath);
+        if (!File.is_open()) {
+            std::cerr << "Failed to open file: " + fpath << std::endl;
+            return;
+        }
+        std::stringstream buf;
+        buf << File.rdbuf();
+        std::string FileContents = buf.str();
+        File.close();
+        NeuronType cType = InputNeuron;
+        float ActivationThreshold;
+        std::string working_on = "";
+        int o;
+        for (int i = 0; i < FileContents.length(); i++) {
+            char c = FileContents[i];
+            bool breakout = false;
+            switch (c) {
+                case '|':
+                    working_on = "";
+                    switch (cType) {
+                        case InputNeuron:
+                            cType = HiddenNeuron;
+                            break;
+                        case HiddenNeuron:
+                            cType = OutputNeuron;
+                            break;
+                        case OutputNeuron:
+                            throw std::invalid_argument("Invalid file formatting for importing of weights for neural network. File path: " + fpath);
+                    }
+                case ' ':
+                    if (working_on.empty()) continue; else if (ActivationThreshold > 0) {
+                        switch (cType) {
+                            case InputNeuron:
+                                In.emplace_back(cType, ActivationThreshold, std::stoi(working_on));
+                                break;
+                            case HiddenNeuron:
+                                Hidden.emplace_back(cType, ActivationThreshold, std::stoi(working_on));
+                                break;
+                            case OutputNeuron:
+                                Out.emplace_back(cType, ActivationThreshold, std::stoi(working_on));
+                                break;
+                        }
+                    } else ActivationThreshold = std::stoi(working_on);
+                    working_on = "";
+                    break;
+                case '~':
+                    breakout = true;
+                    o = i;
+                    break;
+                default:
+                    working_on += c;
+            }
+            if (breakout) break;
+        }
+        working_on = "";
+        int a;
+        for (int i = o; i < FileContents.length(); i++) {
+            char c = FileContents[i];
+            switch (c) {
+                case '>':
+                    a = std::stoi(working_on);
+                    working_on = "";
+                    break;
+                case ' ': {
+                    int b = std::stoi(working_on);
+                    working_on = "";
+                    if (a < In.size()) In[a].Axon.emplace_back(&In[a], &Hidden[b]);
+                    else if (b > Hidden.size()) Hidden[a].Axon.emplace_back(&Hidden[a], &Out[b-Hidden.size()]);
+                    else Hidden[a].Axon.emplace_back(&Hidden[a], &Hidden[b]);
+                    break;
+                }
+                default:
+                    working_on += c;
+            }
+        }
+    }
+    void Export(std::string fpath) {
+        std::ofstream File(fpath);
+        if (!File.is_open()) {
+            std::cerr << "Error creating or opening file with path: " + fpath << std::endl;
+            return;
+        }
+        for (Neuron n : In) File << n.ActivationThreshold << " " << n.LeakRate << " ";
+        File << "| ";
+        for (Neuron n : Hidden) File << n.ActivationThreshold << " " << n.LeakRate << " ";
+        File << "| ";
+        for (Neuron n : Out) File << n.ActivationThreshold << " " << n.LeakRate << " ";
+        File << "~ ";
+        for (int i = 0; i < In.size(); i++) for (AxonTerminalComponent ac : In[i].Axon) File << i << ">" << std::distance(Hidden.begin(), std::find(Hidden.begin(), Hidden.end(), *ac.Out)) << " ";
+        for (int i = 0; i < Hidden.size(); i++) for (AxonTerminalComponent ac : Hidden[i].Axon) {
+            auto it = std::find(Hidden.begin(), Hidden.end(), *ac.Out);
+            if (it != Hidden.end()) File << i + In.size() << ">" << std::distance(Hidden.begin(), std::find(Hidden.begin(), Hidden.end(), *ac.Out)) << " ";
+            else File << i + In.size() << ">" << std::distance(Out.begin(), std::find(Out.begin(), Out.end(), *ac.Out)) + Hidden.size() << " ";
+        }
+        File.close();
+    }
 };
-
 
 #endif //QED_ACTIVENEURALNETWORK_HPP
--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 
 int main() {
     NeuralNetwork NN = NeuralNetwork(5, 5, 25);
+    NN.Export("TrainingOutput.txt");
     return 0;
 }
